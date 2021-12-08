0f72cac37ce6a70671a51bf2e41c8256151bf654
jonasegehrke <jonasegehrke@gmail.com>
2021-12-08 10:21:58 +0100

    login now works with REST

--- js/login.js
+++ js/login.js
@@ -1,6 +1,28 @@
 let userName = prompt("Enter username");
 let password = prompt("Enter your password");
 
-if(userName !== "1" || password !=="1"){
-  window.location.replace("http://127.0.0.1:5500/index.html");
+const login = {
+  username: userName,
+  password: password
 }
+
+const loginURL = `http://localhost:8080`;
+
+async function tryLogin() {
+  
+  await fetch(loginURL + "/login", {
+    method: "POST",
+    body: JSON.stringify(login),
+    headers: { "Content-type": "application/json; charset=UTF-8" }
+  }).then((response) => response.json()).then((data) => {
+    console.log(data)
+
+    if (data === false) {
+      window.location.replace("http://127.0.0.1:5500/index.html");
+    }
+
+  })
+
+}
+
+tryLogin();
